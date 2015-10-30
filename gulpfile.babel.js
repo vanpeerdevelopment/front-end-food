5232272373c55550f07e6738158924a7d607bf0a
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-10-30 22:50:51 +0100

    Deploy to gh-pages after successful build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,14 @@
 import gulp from 'gulp';
+import ghPages from 'gulp-gh-pages';
 
 gulp.task('default', () => {
     return gulp
-        .src('app/**')
-        .pipe(gulp.dest('dist/'));
+        .src('./app/**')
+        .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task("deploy", () => {
+    return gulp
+        .src('./dist/**/*')
+        .pipe(ghPages());
+})
