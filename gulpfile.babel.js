673d03e9c47f83ec956493e5dd2fbe05198b6846
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-09 22:47:52 +0100

    Add gulp clean task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,19 @@
 import gulp from 'gulp';
+import del from 'del';
 import ghPages from 'gulp-gh-pages';
+
+gulp.task('clean', () => {
+    return del('dist/');
+})
 
 gulp.task('default', () => {
     return gulp
-        .src('./app/**/*')
-        .pipe(gulp.dest('./dist/'));
+        .src('app/**/*')
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task("deploy", () => {
     return gulp
-        .src('./dist/**/*')
+        .src('dist/**/*')
         .pipe(ghPages());
 })
