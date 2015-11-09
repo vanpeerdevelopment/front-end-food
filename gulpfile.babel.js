a3585ff236838da9d4a973fa5ae9203ff4173386
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-09 23:03:44 +0100

    Clean before build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,20 @@
 import gulp from 'gulp';
 import del from 'del';
+import runSequence from 'run-sequence';
 import ghPages from 'gulp-gh-pages';
 
 gulp.task('clean', () => {
     return del('dist/');
 })
 
-gulp.task('default', () => {
+gulp.task('build', () => {
     return gulp
         .src('app/**/*')
         .pipe(gulp.dest('dist/'));
+})
+
+gulp.task('default', (callback) => {
+    runSequence('clean', 'build', callback);
 });
 
 gulp.task("deploy", () => {
