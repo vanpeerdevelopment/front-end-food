8534f931eae58f822a028ca6c14442a91a3fd0d7
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-11 14:17:09 +0100

    Move app to src/app + Add gulp clean:dev task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,48 @@
 import gulp from 'gulp';
+import runSequence from 'run-sequence';
 import del from 'del';
 import mainBowerFiles from 'main-bower-files';
-import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
 import ghPages from 'gulp-gh-pages';
+
+let browserSyncServer = browserSync.create('front-end-food	');
+
+/*
+ * main tasks
+ */
+
+gulp.task('default', ['clean:build']);
+
+gulp.task('clean:build', (callback) => {
+    runSequence('clean', 'build', callback);
+});
+
+gulp.task('clean:dev', (callback) => {
+    runSequence('clean', 'dev', callback);
+});
+
+gulp.task("deploy", () => {
+    return gulp
+        .src('dist/**/*')
+        .pipe(ghPages());
+})
+
+
+/*
+ * helper tasks
+ */
 
 gulp.task('clean', () => {
     return del('dist/');
 })
 
+gulp.task('dev', ['build', 'watch', 'serve'])
+
+gulp.task('build', ['build:app', 'build:vendor'])
+
 gulp.task('build:app', () => {
     return gulp
-        .src('app/**/*')
+        .src('src/app/**/*')
         .pipe(gulp.dest('dist/'));
 })
 
@@ -22,14 +54,24 @@ gulp.task('build:vendor', () => {
         .pipe(gulp.dest('dist/vendor/'));
 })
 
-gulp.task('build', ['build:app', 'build:vendor'])
+gulp.task('watch', ['watch:app', 'watch:vendor'])
 
-gulp.task('default', (callback) => {
-    runSequence('clean', 'build', callback);
-});
+gulp.task('watch:app', () => {
+    gulp.watch('src/app/**/*', ['watch:app:build']);
+})
 
-gulp.task("deploy", () => {
-    return gulp
-        .src('dist/**/*')
-        .pipe(ghPages());
+gulp.task('watch:vendor', () => {
+    gulp.watch('bower.json', ['watch:vendor:build']);
+})
+
+gulp.task('watch:app:build', ['build:app'], browserSyncServer.reload)
+
+gulp.task('watch:vendor:build', ['build:vendor'], browserSyncServer.reload)
+
+gulp.task('serve', () => {
+    browserSyncServer.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
 })
