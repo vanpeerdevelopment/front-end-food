1590689517a34b2501d34eadae7bbbe61e376e11
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-16 23:59:12 +0100

    Fix core.config.js by reverting to config function

--- src/app/core/core.config.js
+++ src/app/core/core.config.js
@@ -2,11 +2,11 @@ angular
     .module("app.core")
     .config(configureTemplateMapping);
 
-let configureTemplateMapping = $componentLoaderProvider => {
+function configureTemplateMapping($componentLoaderProvider) {
     let camelCaseToDashCase = name => name.replace(/([A-Z])/g, $1 => "-" + $1.toLowerCase());
 
     $componentLoaderProvider
         .setTemplateMapping(name => "./app/" + camelCaseToDashCase(name) + "/" + camelCaseToDashCase(name) + ".html");
-};
+}
 
 configureTemplateMapping.$inject = ["$componentLoaderProvider"];
