d427d3029ef4e9393455c37276cfee3e89eddda1
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-17 00:41:05 +0100

    Use babel-eslint parser and babel-plugin-eslint rules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,115 +1,114 @@
-import gulp from 'gulp';
-import runSequence from 'run-sequence';
-import del from 'del';
-import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
-import mainBowerFiles from 'main-bower-files';
-import browserSync from 'browser-sync';
-import ghPages from 'gulp-gh-pages';
+import gulp from "gulp";
+import runSequence from "run-sequence";
+import del from "del";
+import babel from "gulp-babel";
+import eslint from "gulp-eslint";
+import mainBowerFiles from "main-bower-files";
+import browserSync from "browser-sync";
+import ghPages from "gulp-gh-pages";
 
-let browserSyncServer = browserSync.create('front-end-food');
+let browserSyncServer = browserSync.create("front-end-food");
 
 /*
  * main tasks
  */
 
-gulp.task('default', ['clean:build']);
+gulp.task("default", ["clean:build"]);
 
-gulp.task('clean:build', (callback) => {
-    runSequence('clean', 'build', callback);
+gulp.task("clean:build", (callback) => {
+    runSequence("clean", "build", callback);
 });
 
-gulp.task('clean:dev', (callback) => {
-    runSequence('clean', 'dev', callback);
+gulp.task("clean:dev", (callback) => {
+    runSequence("clean", "dev", callback);
 });
 
 gulp.task("deploy", () => {
     return gulp
-        .src('dist/**/*')
+        .src("dist/**/*")
         .pipe(ghPages());
-})
-
+});
 
 /*
  * helper tasks
  */
 
-gulp.task('clean', () => {
-    return del('dist/');
-})
+gulp.task("clean", () => {
+    return del("dist/");
+});
 
-gulp.task('dev', ['build:app', 'build:vendor', 'watch', 'serve'])
+gulp.task("dev", ["build:app", "build:vendor", "watch", "serve"]);
 
-gulp.task('build', ['build:app', 'build:vendor', 'lint'])
+gulp.task("build", ["build:app", "build:vendor", "lint"]);
 
-gulp.task('build:app', ['build:app:js', 'build:app:html', "build:app:cname"])
+gulp.task("build:app", ["build:app:js", "build:app:html", "build:app:cname"]);
 
-gulp.task('build:app:js', () => {
+gulp.task("build:app:js", () => {
     return gulp
-        .src(['src/app/**/*.js'], {
-            base: 'src'
+        .src(["src/app/**/*.js"], {
+            base: "src"
         })
         .pipe(babel({
-            presets: ['es2015']
+            presets: ["es2015"]
         }))
-        .pipe(gulp.dest('dist/'));
-})
+        .pipe(gulp.dest("dist/"));
+});
 
-gulp.task('build:app:html', () => {
+gulp.task("build:app:html", () => {
     return gulp
-        .src(['src/index.html', 'src/app/**/*.html'], {
-            base: 'src'
+        .src(["src/index.html", "src/app/**/*.html"], {
+            base: "src"
         })
-        .pipe(gulp.dest('dist/'));
-})
+        .pipe(gulp.dest("dist/"));
+});
 
-gulp.task('build:app:cname', () => {
+gulp.task("build:app:cname", () => {
     return gulp
-        .src(['src/CNAME'])
-        .pipe(gulp.dest('dist/'));
-})
+        .src(["src/CNAME"])
+        .pipe(gulp.dest("dist/"));
+});
 
-gulp.task('build:vendor', ['build:vendor:bower', 'build:vendor:lib'])
+gulp.task("build:vendor", ["build:vendor:bower", "build:vendor:lib"]);
 
-gulp.task('build:vendor:bower', () => {
+gulp.task("build:vendor:bower", () => {
     return gulp
         .src(mainBowerFiles({
             checkExistence: true
         }))
-        .pipe(gulp.dest('dist/vendor/'));
-})
+        .pipe(gulp.dest("dist/vendor/"));
+});
 
-gulp.task('build:vendor:lib', () => {
+gulp.task("build:vendor:lib", () => {
     return gulp
-        .src('src/lib/**/*')
-        .pipe(gulp.dest('dist/vendor/'));
-})
+        .src("src/lib/**/*")
+        .pipe(gulp.dest("dist/vendor/"));
+});
 
-gulp.task('lint', () => {
-    return gulp.src('src/app/**/*.js')
+gulp.task("lint", () => {
+    return gulp.src(["gulpfile.babel.js", "src/app/**/*.js"])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-})
+});
 
-gulp.task('watch', ['watch:app', 'watch:vendor'])
+gulp.task("watch", ["watch:app", "watch:vendor"]);
 
-gulp.task('watch:app', () => {
-    gulp.watch(['src/app/**/*', 'src/CNAME', 'src/index.html'], ['watch:app:build']);
-})
+gulp.task("watch:app", () => {
+    gulp.watch(["src/app/**/*", "src/CNAME", "src/index.html"], ["watch:app:build"]);
+});
 
-gulp.task('watch:vendor', () => {
-    gulp.watch(['bower.json', 'src/lib/**/*'], ['watch:vendor:build']);
-})
+gulp.task("watch:vendor", () => {
+    gulp.watch(["bower.json", "src/lib/**/*"], ["watch:vendor:build"]);
+});
 
-gulp.task('watch:app:build', ['build:app'], browserSyncServer.reload)
+gulp.task("watch:app:build", ["build:app"], browserSyncServer.reload);
 
-gulp.task('watch:vendor:build', ['build:vendor'], browserSyncServer.reload)
+gulp.task("watch:vendor:build", ["build:vendor"], browserSyncServer.reload);
 
-gulp.task('serve', () => {
+gulp.task("serve", () => {
     browserSyncServer.init({
         server: {
-            baseDir: 'dist/'
+            baseDir: "dist/"
         }
     });
-})
+});
