7ef5cc3763761457c600e71398640b679fc6cb52
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-23 21:48:28 +0100

    Change order of app importd

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,9 +1,9 @@
-import {disableDebugInfo, enableHtml5Mode} from "app.config";
-import RouteController from "app.route";
 import CoreModule from "core/core.module";
 import WidgetsModule from "widgets/widgets.module";
 import LayoutModule from "layout/layout.module";
 import HomeModule from "home/home.module";
+import {disableDebugInfo, enableHtml5Mode} from "app.config";
+import RouteController from "app.route";
 
 export default angular
     .module("app", [
