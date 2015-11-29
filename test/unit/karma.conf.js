d80e374d98aa8a7d095532ed9c06cf24d141deb2
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-11-29 14:02:19 +0100

    Use ES2015 for karma tests

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,8 +3,23 @@
 module.exports = function configure(config) {
     config.set({
         basePath: "../..",
-        files: ["test/unit/**/*.spec.js"],
+        files: [
+            "node_modules/babel-polyfill/dist/polyfill.js",
+            "test/unit/**/*.spec.js"
+        ],
         browsers: ["PhantomJS"],
+        preprocessors: {
+            "test/unit/**/*.spec.js": ["babel"]
+        },
+        babelPreprocessor: {
+            options: {
+                presets: ["es2015"],
+                sourceMap: "inline"
+            },
+            sourceFileName: function sourceFileName(file) {
+                return `/sources/${file.originalPath.substring(file.originalPath.indexOf("test/unit/"))}`;
+            }
+        },
         frameworks: [
             "mocha",
             "chai-sinon"
@@ -12,6 +27,7 @@ module.exports = function configure(config) {
         reporters: ["mocha"],
         plugins: [
             "karma-phantomjs-launcher",
+            "karma-babel-preprocessor",
             "karma-mocha",
             "karma-chai-sinon",
             "karma-mocha-reporter"
--- test/unit/karma.spec.js
+++ test/unit/karma.spec.js
@@ -1,22 +1,22 @@
 /* eslint no-unused-expressions: 0 */
 
-describe("Karma", function karma() {
-    it("should work with Mocha", function shouldMocha() {});
+describe("Karma", () => {
+    it("should work with Mocha", () => {});
 
-    it("should work with Chai", function shouldChai() {
-        [1, 2, 3].should.have.length(3);
-    });
+    it("should work with Chai", () =>
+        [1, 2, 3].should.have.length(3)
+    );
 
-    it("should work with Sinon", function shouldSinon() {
-        var addMock = sinon.spy();
+    it("should work with Sinon", () => {
+        let addMock = sinon.spy();
 
         addMock(1, 2);
 
         addMock.calledWith(1, 2).should.be.ok;
     });
 
-    it("should work with Sinon-Chai", function shouldSinonChai() {
-        var addMock = sinon.spy();
+    it("should work with Sinon-Chai", () => {
+        let addMock = sinon.spy();
 
         addMock(1, 2);
 
