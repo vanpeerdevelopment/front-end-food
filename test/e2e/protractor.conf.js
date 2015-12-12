cdd157139f0ade534027e7ad59100b0333fa1f5c
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-12-12 18:09:04 +0100

    Use protractor mocha

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -1,7 +1,12 @@
 /* globals exports */
 
 exports.config = {
-    framework: "jasmine",
+    framework: "mocha",
+    mochaOpts: {
+        reporter: "spec",
+        slow: 3000,
+        timeout: 5000
+    },
     seleniumServerJar: "../../node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar",
     specs: ["protractor.spec.js"],
     capabilities: {
--- test/e2e/protractor.spec.js
+++ test/e2e/protractor.spec.js
@@ -1,9 +1,18 @@
+/* globals require */
+/* eslint no-var:0 no-shadow:0, vars-on-top: 0 */
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 describe("Front End Food", function frontEndFood() {
     it("should have a title", function shouldHaveTitle() {
         browser.get("http://localhost:3000");
 
-        expect(browser.getTitle()).toEqual("Front End Food");
-        expect(element(by.binding("home.title")).getText()).toEqual("Front End Food");
-        expect(element(by.tagName("p")).getText()).toEqual("Angular is working: 1 + 1 = 2");
+        expect(browser.getTitle()).to.eventually.equal("Front End Food");
+        expect(element(by.binding("home.title")).getText()).to.eventually.equal("Front End Food");
+        expect(element(by.tagName("p")).getText()).to.eventually.equal("Angular is working: 1 + 1 = 2");
     });
 });
