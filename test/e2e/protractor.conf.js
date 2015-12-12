af3dc11ccf5bd1203f2c4e3de4212091741ad15f
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-12-12 17:11:55 +0100

    Start selenium server automatically

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -2,7 +2,7 @@
 
 exports.config = {
     framework: "jasmine",
-    seleniumAddress: "http://localhost:4444/wd/hub",
+    seleniumServerJar: "../../node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar",
     specs: ["protractor.spec.js"],
     capabilities: {
         browserName: "firefox"
