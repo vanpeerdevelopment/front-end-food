8cb7f3b3fddb716c59523caa82b8332dcd174f78
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2015-12-20 20:01:33 +0100

    Run e2e tests locally on chrome

--- test/e2e/config/protractor.local.config.js
+++ test/e2e/config/protractor.local.config.js
@@ -4,11 +4,7 @@
 var ProtractorConfigBuilder = require("./protractor.common.config");
 
 var localSeleniumServer = "../../../node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar";
-var firefoxCapability = {
-    browserName: "firefox"
-};
 
 exports.config = new ProtractorConfigBuilder()
     .withSeleniumServerJar(localSeleniumServer)
-    .withCapabilities(firefoxCapability)
     .build();
